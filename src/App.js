1c8a9d895ee3ff37b66e292d7f584fda7cc7b4d6
Mario Lugo <mlugo23699@gmail.com>
2022-09-15 12:32:26 -0700

    created DisplayEntries and AddForm components and started insantiation of App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [entries, setEntries] = useState([{name: "David Lagrange", post: "text"}, {name: "JJ Vega", post: "text"}, {name: "Nevin Seibel", post: "text"}])
+
+  function addNewEntry(entry){
+
+    let tempEntries = [entry, ...entries];
+
+    setEntries(tempEntries);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddEntryForm addNewEntryProperty = {addNewEntry}/>
+      <DisplayEntries parentEntries = {entries}/>
     </div>
   );
 }
